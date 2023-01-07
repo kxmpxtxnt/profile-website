6ff5904d09d170d867d0cb5e605d8977e3ea2ca1
Paul Kindler <paul.kindler@staffbase.com>
2023-01-07 23:25:17 +0100

    try to add fancy system color sync 🐛

--- src/Profile.tsx
+++ src/Profile.tsx
@@ -3,44 +3,52 @@ import {useState} from "react";
 import './style/fontstyle.css';
 import './style/profile/profile.css'
 
-let isDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
-
-export const colorModeStyle = {
-    backgroundColor: isDarkMode ? "#2D2424" : "#E0C097",
-    color: isDarkMode ? "#E0C097" : "#2D2424",
-}
-
 function profilePage() {
-    document.body.style.backgroundColor = colorModeStyle.backgroundColor
-    document.body.style.color = colorModeStyle.color
 
-    const enableHover = () => {
-        setSublineText(githubLine)
-    };
+  const [darkMode, setDarkMode] = useState(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches)
 
-    const disableHover = () => {
-        setSublineText(defaultSubline)
-    };
+  const changeColorTheme = () => {
+    setDarkMode(!darkMode)
+  };
 
-    const defaultSubline = <div>I'm a german 🇩🇪 <u>backend developer</u> 🧑🏽 <b onMouseEnter={enableHover}> → [hover]</b></div>
-    const githubLine = <a style={colorModeStyle} href={"https://github.com/kxmpxtxnt"}>Checkout my <u>Github</u> 🧑🏽‍💻 <b onMouseLeave={disableHover}>→ [click]</b></a>
+  const colorMode = {
+    backgroundColor: darkMode ? "#2D2424" : "#E0C097",
+    color: darkMode ? "#E0C097" : "#2D2424",
+  }
 
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    const [getSublineText, setSublineText] = useState(defaultSubline)
+  document.body.style.backgroundColor = colorMode.backgroundColor
+  document.body.style.color = colorMode.color
 
+  const enableHover = () => {
+    setSublineText(githubLine)
+  };
 
-    return (
-        <div className={"profile"}>
-            <div className={"header"}>
-                <div className={"headline"}>
-                    <b>Hey 👋 - my name is <u>Paul</u> <a style={colorModeStyle} href={"https://upload.wikimedia.org/wikipedia/commons/7/70/En-us-Paul.oga"}>→ [paʊ̯l]</a></b>
-                </div>
-                <div className={"subheadline"} >
-                    {getSublineText}
-                </div>
-            </div>
+  const disableHover = () => {
+    setSublineText(defaultSubline)
+  };
+
+  const defaultSubline = <div>I'm a german 🇩🇪 <u>backend developer</u> 🧑🏽<b onMouseEnter={enableHover}> → [hover]</b>
+  </div>
+
+  const githubLine = <a style={colorMode} href={"https://github.com/kxmpxtxnt"}>Checkout my <u>Github</u> 🧑🏽‍💻<b onMouseLeave={disableHover}> → [click]</b></a>
+
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [getSublineText, setSublineText] = useState(defaultSubline)
+
+  return (
+    <div className={"profile"}>
+      <div className={"header"}>
+        <div className={"headline"}>
+          <b>Hey 👋 - my name is <u>Paul</u>
+            <a style={colorMode} href={"https://upload.wikimedia.org/wikipedia/commons/7/70/En-us-Paul.oga"}> →
+              [paʊ̯l]</a></b>
         </div>
-    );
+        <div className={"subheadline"}>
+          {getSublineText}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default profilePage
